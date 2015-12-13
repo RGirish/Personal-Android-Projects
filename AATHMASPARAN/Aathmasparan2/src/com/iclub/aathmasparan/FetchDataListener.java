b07ab7a4315cdaadc45e255ec22a4c61069c333a
Girish Raman <rgirish1994@gmail.com>
2015-12-13 09:15:29 +0530

    Removed/ Renamed/ Moved some folders

--- AATHMASPARAN/Aathmasparan2/src/com/iclub/aathmasparan/FetchDataListener.java
+++
@@ -1,8 +0,0 @@
-package com.iclub.aathmasparan;
-
-public interface FetchDataListener 
-{
-	public void onFetchComplete();
-    public void onFetchFailure(String msg);
-	public void onFetchComplete(String string);
-}
--- COGNIT14/Cognit14Registration/app/src/main/java/com/iclub/cognit14registration/FetchDataListener.java
+++
@@ -1,8 +0,0 @@
-package com.iclub.cognit14registration;
-
-public interface FetchDataListener 
-{
-	public void onFetchComplete();
-    public void onFetchFailure(String msg);
-	public void onFetchComplete(String string);
-}
